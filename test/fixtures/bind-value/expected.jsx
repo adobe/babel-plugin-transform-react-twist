3348b3cdf811523680f834ad30986da361850b25
Marcus Cavanaugh <m@mcav.com>
2018-01-18 12:49:21 -0800

    Allow both `bind:value` and `onChange` handlers. (#7)
    Fixes #6.

--- test/fixtures/bind-value/expected.jsx
+++ test/fixtures/bind-value/expected.jsx
@@ -2,6 +2,8 @@
     <input value={ x.value } />
     <input value={x.value} onChange={ev => x.value = ev.target.value} />
     <input type="text" value={value} onChange={ev => value = ev.target.value} />
+    <input value={x.value} onChange={ev => (x.value = ev.target.value, (() => unrelatedHandler())(ev))} />
+    <input value={x.value} onChange={ev => (x.value = ev.target.value, (ev => x.value = ev.target.value)(ev))} />
     <select value={value} onChange={ev => value = ev.target.value}>
         <option value="a">A</option>
         <option value="b">B</option>
